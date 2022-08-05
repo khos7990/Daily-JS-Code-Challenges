431c324fe31982d6a4e4bf2be3c7b8937713d04a
ishan <ishan.khosla22@gmail.com>
2022-08-04 23:28:33 -0400

    daily challenge 2

--- challenges.js
+++ challenges.js
@@ -11,13 +11,29 @@
 
 //ANSWER
 function likes(names) {
-    if (names.length === 0) return "no one likes this"
-  if(names.length === 1) return names[0] + ' likes this' 
-  if (names.length > 1 && names.length< 3) return(names[0] + ' ' + 'and '  + names[1] + ' like this')
-  if (names.length > 2 && names.length < 4) return(names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this' )
+  if (names.length === 0) return "no one likes this";
+  if (names.length === 1) return names[0] + " likes this";
+  if (names.length > 1 && names.length < 3)
+    return names[0] + " " + "and " + names[1] + " like this";
+  if (names.length > 2 && names.length < 4)
+    return names[0] + ", " + names[1] + " and " + names[2] + " like this";
   if (names.length > 3) {
-    let others = names.length - 2
-    return names[0] + ', ' + names[1] + ' and ' + others + ' others like this'
+    let others = names.length - 2;
+    return names[0] + ", " + names[1] + " and " + others + " others like this";
   }
 }
 
+// 2. A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+//Answer
+function isPangram(string) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let lowercaseStr = string.toLowerCase();
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (lowercaseStr.indexOf(alphabet.charAt(i)) === -1) return false;
+  }
+  return true;
+}
