7f3d8daac4d8459cd6f6fe8dc67805a0ff36789a
ishan <ishan.khosla22@gmail.com>
2022-08-09 23:51:20 -0400

    daily challenge 3

--- challenges.js
+++ challenges.js
@@ -37,3 +37,11 @@ function isPangram(string) {
   }
   return true;
 }
+
+//3. Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+//It should remove all values from list a, which are present in list b keeping their order.
+//If a value is present in b, all of its occurrences must be removed from the other:
+
+function arrayDiff(a, b) {
+  return a.filter(e => !b.includes(e))
+  }
