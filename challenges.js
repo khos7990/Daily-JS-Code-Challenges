d01dcac616a6fc313f716ea3f3e31975119b759f
ishan <ishan.khosla22@gmail.com>
2022-08-30 22:18:39 -0400

    q7 remove ends

--- challenges.js
+++ challenges.js
@@ -166,3 +166,14 @@ function isPrime(num) {
 //   }
 //   return true;
 // }
+
+//7. // - Write a function called removeEnds that accepts a single string argument, then returns the a string with the first and last characters removed.
+// - If the length of the string argument is less than 3, return an empty string.
+
+//Answer
+
+function removeEnds(str) {
+  if (str.length < 3) return "";
+
+  return str.substring(1, str.length - 1);
+}
