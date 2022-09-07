61f796e7da2cdb7d8aeccb395d498c7d850324a9
ishan <ishan.khosla22@gmail.com>
2022-09-06 20:55:50 -0400

    sorted array q8

--- challenges.js
+++ challenges.js
@@ -211,3 +211,34 @@ function solution(str) {
 //     }
 //   return result;
 // }
+
+
+//9. You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+//Answer
+function sortArray(array) {
+  var odds = [];
+  //loop, if it's odd, push to odds array
+  for (var i = 0; i < array.length; ++i) {
+    if (array[i]%2 !== 0) {
+      odds.push(array[i]);
+    }
+  }
+  //sort odds from smallest to largest
+  odds.sort(function(a,b){
+    return a-b;
+  });
+  
+  //loop through array, replace any odd values with sorted odd values
+  for (var j = 0; j < array.length; ++j) {
+    if (array[j]%2 !== 0) {
+      array[j] = odds.shift();
+    }
+  }
+  
+ return array
+
+ //best solution
+//  function sortArray(array) {
+//   const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+//   return array.map((x) => x % 2 ? odd.shift() : x);
+// }
